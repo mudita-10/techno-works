f87e9071658cf3999bff06bb0f73e9d8aab0bc45
Mudita Bhowmick <91493796+mudita-10@users.noreply.github.com>
2021-10-27 14:47:30 +0530

    Add files via upload

---
+++ C-codes/28_pattern3.c
@@ -0,0 +1,28 @@
+/*
+
+Created by: Mudita Bhowmick
+Program to print the following pattern:1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2
+1
+.. n rows
+
+*/
+
+#include <stdio.h>
+int main()
+{
+   int i,j,rows;
+   printf("\nEnter the number of rows:\n");
+   scanf("%d",&rows);
+   for (i=rows;i>=1;--i)
+   {
+      for (j = 1; j <= i; ++j)
+      {
+        printf("%d ",j);
+      }
+      printf("\n");
+   }
+   return 0;
+}
