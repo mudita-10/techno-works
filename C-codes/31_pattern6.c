f2f1c6f8a329b5fcf229a56e3453b04b9d6d2f19
Mudita Bhowmick <91493796+mudita-10@users.noreply.github.com>
2021-10-31 22:00:41 +0530

    Add files via upload

---
+++ C-codes/31_pattern6.c
@@ -0,0 +1,39 @@
+/*
+
+Created by: Mudita Bhowmick
+Program to print the following pattern:
+    *
+   * *
+  *   *
+ *     *
+*********
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i,j,rows;
+    printf("Enter number of rows:\n");
+    scanf("%d", &rows);
+    for(i=1;i<=rows; i++)
+    {
+        for(j=i;j<rows;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=(2*i-1);j++)
+        {
+            if(i==rows||j==1||j==(2*i-1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
